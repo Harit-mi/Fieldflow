6dbe4a8b796d531247157486a331726af257eaf8
Harit Mishra <haritmishra@example.com>
2026-09-08 23:28:21 +0530

    chore: remove test script

--- test-invalid-url.js
+++
@@ -1,7 +0,0 @@
-const { createBrowserClient } = require('@supabase/ssr');
-try {
-  const client = createBrowserClient('dummy', 'dummy-key');
-  console.log("Client created successfully");
-} catch (e) {
-  console.log("Error creating client:", e.message);
-}
